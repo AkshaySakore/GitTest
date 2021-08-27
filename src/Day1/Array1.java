d19ca14882a3b6b72c18d6ace2ca45bb7d5fe8f9
Akshay <Akshay@DESKTOP-COTM8MH>
2021-08-27 16:59:21 +0530

    First Git Add

---
+++ src/Day1/Array1.java
@@ -0,0 +1,33 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		
+		int [][] arr=new int[2][3];
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
+		
+		Scanner sc=new Scanner(System.in);
+		for(int r=0;r<arr.length;r++)
+		{
+			System.out.println("enter element for row"+r);
+			for(int c=0;c<arr[r].length;c++)
+			{
+				arr[r][c]=sc.nextInt();
+			}
+		}
+		for(int r=0;r<arr.length;r++)
+		{
+			for(int c=0;c<arr[r].length;c++)
+			{
+				System.out.print(arr[r][c]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
